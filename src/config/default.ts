2e354f729ce36a23b2a7a703afdb23354dc5256b
thomas <thomas.xbh@gmail.com>
2024-08-22 16:10:22 +0800

    完善配置

---
+++ src/config/default.ts
@@ -0,0 +1,87 @@
+export default () => {  
+  if (process.env.NODE_ENV === "development") {
+    return {
+      // 服务器运行地址及端口
+      APP_HOST: "localhost",
+      APP_PORT: 9090,
+      APP_HTTP: "http",
+      // mysql
+      MYSQL_HOST: "localhost",
+      MYSQL_PORT: 3306,
+      MYSQL_USER: "root",
+      MYSQL_PWD: "admin123",
+      MYSQL_DB: "leno_admin",
+      // JWT
+      JWT_SECRET: "lenoAdmin",
+      JWT_REFRESH_SECRET: "refreshToken",
+      // redis
+      REDIS_PORT: 6379,
+      REDIS_HOST: "localhost",
+      REDIS_PASSWORD: "admin123",
+      REDIS_DB: 0,
+      // 图片路径
+      IMG_URL: "http://localhost:9090/uploads/",
+      // 是否开启禁止操作修改删除类接口（此功能仅用于上线网页预览使用）
+      STOP_EDIT: false,
+      // 日志时区
+      LOG_TIME: "zh-CN",
+    };
+  }
+  if (process.env.NODE_ENV === "docker-desktop") {
+    return {
+      // 服务器运行地址及端口
+      APP_HOST: "localhost",
+      APP_PORT: 9090,
+      APP_HTTP: "http",
+      // mysql
+      MYSQL_HOST: "mysql",
+      MYSQL_PORT: 4000,
+      MYSQL_USER: "root",
+      MYSQL_PWD: "Aa363689;",
+      MYSQL_DB: "leno_admin",
+      // JWT
+      JWT_SECRET: "lenoAdmin",
+      JWT_REFRESH_SECRET: "refreshToken",
+      // redis
+      REDIS_PORT: 4100,
+      // REDIS_HOST: 'host.docker.internal',
+      REDIS_HOST: "host.docker.internal",
+      REDIS_PASSWORD: "admin123",
+      REDIS_DB: 0,
+      // 图片路径
+      IMG_URL: "http://localhost:9090/uploads/",
+      // 是否开启禁止操作修改删除类接口（此功能仅用于上线网页预览使用）
+      STOP_EDIT: false,
+      // 日志时区
+      LOG_TIME: "zh-CN",
+    };
+  }
+  if (process.env.NODE_ENV === "production") {
+    return {
+      // 服务器运行地址及端口
+      APP_HOST: "localhost",
+      APP_PORT: 9000,
+      APP_HTTP: "http",
+      // mysql
+      MYSQL_HOST: "mysql",
+      MYSQL_PORT: 3306,
+      MYSQL_USER: "root",
+      MYSQL_PWD: "Aa363689;",
+      MYSQL_DB: "leno_admin",
+      // JWT
+      JWT_SECRET: "lenoAdmin",
+      JWT_REFRESH_SECRET: "refreshToken",
+      // redis
+      REDIS_PORT: 6379,
+      REDIS_HOST: "redis",
+      REDIS_PASSWORD: "admin123",
+      REDIS_DB: 0,
+      // 图片路径
+      IMG_URL: "http://zhaowenchao.top/uploads/",
+      // 是否开启禁止操作修改删除类接口（此功能仅用于上线网页预览使用）
+      STOP_EDIT: true,
+      // 日志时区
+      LOG_TIME: "zh-CN",
+    };
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import app from './app/index';
+import env from './config/default';
 
 
 
-app.listen(3000, () => {
-    console.log(`服务器启动成功,running`);
-  });
+const { APP_PORT, APP_HOST, APP_HTTP } = env();
+
+app.listen(APP_PORT, () => {
+  console.log(`服务器启动成功,running \x1b[38;2;39;184;219m${APP_HTTP}://${APP_HOST}:${APP_PORT}\x1b[0m`);
+});
