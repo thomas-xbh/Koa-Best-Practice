97dbd663da89932a6dbadbdd54d4301f64023620
thomas-xbh <thomas.xbh@gmail.com>
2024-09-03 11:45:22 +0800

    Delete src/config directory

--- src/config/index.ts
+++
@@ -1,4 +0,0 @@
-// log 白名单
-export const logWhites = ['/user/login', '/user/register', '/user/captchaImage', '/user/logout'];
-// 权限 白名单
-export const authWhites = ['/user/login', '/user/register', '/user/captchaImage', '/system/config/configKey'];
